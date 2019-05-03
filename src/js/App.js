cc917c71b59d42ef4d36850440053d0deaea1188
Saifur Rahman <safrrhmn@hotmail.com>
2019-05-03 15:19:06 -0400

    added header component for navbar

--- src/js/App.js
+++ src/js/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from '../resource/logo.svg';
 import '../css/App.css';
+import HeaderComponent from './HeaderComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+
+
+    return (
+      <div className="App" >
+        <HeaderComponent></HeaderComponent>
+      </div>
+    );
+
+  }
 }
+
 
 export default App;
