bde517510a9084840853ff1209f78d1ed9e95ea0
Saifur Rahman <safrrhmn@hotmail.com>
2019-08-25 18:04:47 -0400

    first commit

---
+++ build/precache-manifest.2ebdc4670b82258eaade46e3ffe5192c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512b0d16dd618ca5ae24e5093c0483b2",
+    "url": "/saifurtech-website/index.html"
+  },
+  {
+    "revision": "757574b6155bf3d85f13",
+    "url": "/saifurtech-website/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/saifurtech-website/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "757574b6155bf3d85f13",
+    "url": "/saifurtech-website/static/js/main.7172c68d.chunk.js"
+  },
+  {
+    "revision": "827ebfb868f6880a2f3e",
+    "url": "/saifurtech-website/static/js/runtime~main.8c20c0e0.js"
+  }
+]);
