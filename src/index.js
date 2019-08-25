dc42bdd69add60bee0f5cabb5ff7afeeab39c838
Saifur Rahman <safrrhmn@hotmail.com>
2019-08-25 19:34:52 -0400

    new repo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './js/App';
-import * as serviceWorker from './js/serviceWorker';
-
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
